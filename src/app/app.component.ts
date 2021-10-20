f6d455a7c0b722a1e2668f0d7b0a8063b6097d45
Виктор <vitek_zema@mail.ru>
2021-10-20 22:22:34 +0300

    api service test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ApiClientService} from "./api/api-client.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TestFrontend';
+
+  constructor(protected api: ApiClientService) {
+  }
+
+  ngOnInit(): void {
+    this.api.getRoot(items => {
+      console.log(items);
+    });
+  }
 }
