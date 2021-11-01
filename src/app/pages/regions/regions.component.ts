66dca18587becfa7f9cb54511c6ecf7bbd81520e
Виктор <vitek_zema@mail.ru>
2021-11-01 09:23:22 +0300

    testing code

--- src/app/pages/regions/regions.component.ts
+++ src/app/pages/regions/regions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import {ApiClientService} from "../../api/api-client.service";
 
 @Component({
   selector: 'app-regions',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiClientService) { }
+
+  updateFlag = false;
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+
+    title: {
+      text: 'Data from server'
+    },
+
+    exporting: {
+      enabled: false
+    },
+
+    series: [
+      {
+        "type": "line",
+        "data": []
+      }
+    ]
+  }
 
   ngOnInit(): void {
+    this.api.getCountry(
+      {"type": "line", "discrete": true, "x_field": "Year", "y_field": "Value"},
+      {"country": ["United Kingdom"], "year": {"2020": "less_or_equal"}, "var": ["pop"]},
+      items => {
+        this.chartOptions.series = items;
+        this.updateFlag = true;
+      });
   }
 
 }
