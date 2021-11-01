a3c0dc69c472167c304861704f2a369ca91e63be
Виктор <vitek_zema@mail.ru>
2021-11-01 08:26:18 +0300

    add routes

---
+++ src/app/pages/int-org/int-org.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-int-org',
+  templateUrl: './int-org.component.html',
+  styleUrls: ['./int-org.component.css']
+})
+export class IntOrgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/subregions/subregions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subregions',
+  templateUrl: './subregions.component.html',
+  styleUrls: ['./subregions.component.css']
+})
+export class SubregionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
