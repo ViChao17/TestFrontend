5c3ee716d3acecdcb76786a91954fa873ef4db12
Виктор <vitek_zema@mail.ru>
2021-11-06 20:02:45 +0300

    final

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -31,7 +31,7 @@ export class MainComponent implements OnInit {
     title: {
       verticalAlign: 'middle',
       floating: true,
-      text: '$ 500.000.00',
+      text: 'Some text',
     },
     legend: {
       enabled: false
@@ -148,6 +148,25 @@ export class MainComponent implements OnInit {
 
 
   lineChartOptions: Highcharts.Options = {
+    chart: {
+      events: {
+        load: function () {
+
+          // set up the updating of the chart each second
+          let series = this.series[0];
+          setInterval(function () {
+            let x = (new Date()).getTime(), // current time
+              y = Math.round(Math.random() * 100);
+            series.addPoint([x, y], true, true);
+          }, 1000);
+        }
+      }
+    },
+
+    time: {
+      useUTC: false
+    },
+
     rangeSelector: {
       buttons: [{
         count: 1,
@@ -174,17 +193,22 @@ export class MainComponent implements OnInit {
     },
 
     series: [{
-      type: 'line',
-      data: [
-        {
-          x: 0,
-          y: 0
-        },
-        {
-          x: 1,
-          y: 2
+      name: 'Random data',
+      // @ts-ignore
+      data: (function () {
+        // generate an array of random data
+        let data = [],
+          time = (new Date()).getTime(),
+          i;
+
+        for (i = -999; i <= 0; i += 1) {
+          data.push([
+            time + i * 1000,
+            Math.round(Math.random() * 100)
+          ]);
         }
-      ]
+        return data;
+      }())
     }]
   }
 
